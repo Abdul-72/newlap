18fc881dd7d77c1188fc780a73cc8f6b76bd68e8
shaik abdul razzak <shaikabdulrazzak295@gmail.com>
2022-02-02 02:38:22 +0530

    v

--- increment.js
+++ increment.js
@@ -1,39 +1,55 @@
-var a = 10 
-var b = 20 
-console.log(b);
-console.log(++a);                   //pre increment 
-console.log(a++);                   //postincrement
+// var a = 10 
+// var b = 20 
+// console.log(b);
+// console.log(++a);                   //pre increment 
+// console.log(a++);                   //postincrement
 
 
-// the below are the examples of the logical operators  , they are , && ,||, ! 
+// // the below are the examples of the logical operators  , they are , && ,||, ! 
 
-var and = 10>5 && 2==2          // logical and operator return true if both side are falling true
-console.log(and);
-var or = 2>3 || 2=="2"         //logical or operator return true if any one of the conditionfalling true
-console.log(or);
-console.log(!or);          //the use of the logical not operator is that it will change the condition to true to false and vise varsa
-
-
+// var and = 10>5 && 2==2          // logical and operator return true if both side are falling true
+// console.log(and);
+// var or = 2>3 || 2=="2"         //logical or operator return true if any one of the conditionfalling true
+// console.log(or);
+// console.log(!or);          //the use of the logical not operator is that it will change the condition to true to false and vise varsa
 
 
-// below are the examples of turnary operators
 
-//the structure of the turnary oprerator is <condition> ? <successoutput> : <failureoutput>
-//if the condition id true it will return the successoutput . if the condition is false it will return the failure output
 
-var rahulIsPresent = true
+// // below are the examples of turnary operators
 
-var condition = rahulIsPresent ? "heispresent": "heisabsent"
-console.log(condition);  
+// //the structure of the turnary oprerator is <condition> ? <successoutput> : <failureoutput>
+// //if the condition id true it will return the successoutput . if the condition is false it will return the failure output
 
-var turnary = 10==2 ? "abdul is present" : "abdul is not present"
-console.log(turnary);
- const div = 10 
- const div2 = 20
+// var rahulIsPresent = true
+
+// var condition = rahulIsPresent ? "heispresent": "heisabsent"
+// console.log(condition);  
+
+// var turnary = 10==2 ? "abdul is present" : "abdul is not present"
+// console.log(turnary);
+//  const div = 10 
+//  const div2 = 20
  
- if (div%10===0 || div2%10===0) {
-     console.log("both are divisible by 10"); 
- } else {console.log("not divisible bt 10");}
+//  if (div%10===0 || div2%10===0) {
+//      console.log("both are divisible by 10"); 
+//  } else {console.log("not divisible bt 10");}
+
+let n = 1234
+var count = 0
+for(i=0;i<=n;i++) {
+    n=parseInt(n/10)
+
+    count=n%10+count
+    
+
+}
+console.log(count);
+
+
+
+
+
 
 
  
